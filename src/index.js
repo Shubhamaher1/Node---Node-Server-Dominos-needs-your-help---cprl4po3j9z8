369ce3eb1e651c902f4961a584b7428de9c7d28f
Shubham Aher <ahers247@gmail.com>
2022-03-10 19:08:15 +0530

    Server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
-}
+  if(req.url=="/welcome"){
+    res.writeHead(200,{
+        "content-type":"text/plain"
+    })
+    res.write("Welcome to Dominos!");
+    res.end("Welcome to Dominos!");
+  }
+  else if(req.url=="/contact"){
+      res.writeHead(200,{
+        "content-type":"application/json"
+      })
+      res.end(JSON.stringify(
+        {
+            phone: "18602100000",
+        email : "guestcaredominos@jublfood.com",
+        }
+      ))
+  }else{
+      res.writeHead(404,{
 
+      })
+      res.end("Not Found")
+  }
+}
+httpServer.listen(8081);
 module.exports = httpServer;
